f6181c964136c91338283bde335b5fabc5174ab6
anshika <annuvyas9511@gmail.com>
2024-12-20 13:26:03 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+#N=input("enter your name")
+#A=int (input("enter your age"))
+#print("your name is",N)
+#print("your age is ",A)
+#print("your name is",N,"your age is",A)
+#sun of two number
+n=7
+b=8
+c=n+b
+print(c)
